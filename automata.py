20132f091d60b242c4a12ff5a630b0be7f12d608
Alberto Medina <blackdrako013@hotmail.com>
2014-02-19 19:50:51 -0600

    Las cosas que hizo Ethan
    Pues ya solo falta lo ultimo, no?

--- automata.py
+++ automata.py
@@ -1,89 +1,83 @@
 #!/usr/bin/python
 
-import sys
+from nodo import Nodo
 
 def imprimirAutomata(inicio):
-	queue = [inicio]
-	while 0 < len(queue):
-		nodo = queue.pop(0)
-		nodo.imprimir()
-		for trans in nodo.transiciones:
-			queue.append(trans[0])
+    indice = 0
+    cola = [inicio]
+    while indice < len(cola):
+        cola[indice].imprimir()
+        for trans in cola[indice].transiciones:
+            if not cola.count(trans[0]):
+                cola.append(trans[0])
+        indice += 1
 
 def expregAThompson(expreg):
-	inicios = []
-	finales = []
-	n = len(expreg)
-	
-	for i in range(n):
-		if expreg[i] == '|':
-			nodo_union_inicio = Nodo()		
-			nodo_union_final = Nodo(True)
-			nodo_inicio_grafo1 = inicio.pop()
-			nodo_final_grafo1 = final.pop()			
-			nodo_final_grafo1.esFinal = False			
-			nodo_inicio_grafo2 = inicio.pop()
-			nodo_final_grafo2 = final.pop()	
-			nodo_final_grafo2.esFinal(False)
-			nodo_union_inicio.agregar_transicion(nodo_inicio_grafo1, '@')						
-			nodo_union_inicio.agregar_transicion(nodo_inicio_grafo2, '@')
-			nodo_final_grafo1.agregar_transicion(nodo_union_final, '@')
-			nodo_final_grafo2.agregar_transicio(nodo_union_final, '@')	
-			inicio.append(nodo_union_inicio)
-			final.append(nodo_union_final)
-			
-		elif expreg[i] == '-':
-			nodoInicioB = inicios.pop()
-			nodoInicioA = inicios.pop()
-			nodoFinalB = finales.pop()
-			nodoFinalA = finales.pop()
-			
-			nodoFinalA.esFinal = False;
-			for transicion in nodoInicioB.transiciones:
-				nodoFinalA.agregarTransicion(transicion[0], transicion[1])
-			del nodoInicioB
-			
-			inicios.append(nodoInicioA)
-			finales.append(nodoFinalB)
+    n = len(expreg)
+    if n == 0:
+        return Nodo(True)
+    
+    inicios = []
+    finales = []
 
-		elif expreg[i] == '+':
-			nodo_positiva_inicio = Nodo()
-			nodo_positiva_final = Nodo(True)
-			nodo_inicio_grafo = inicio.pop()
-			nodo_final_grafo = final.pop()
-			nodo_final_grafo.esFinal(False)
-			nodo_final_grafo.agregar_transicion(nodo_positiva_final, '@')				
-			nodo_final_grafo.agregar_transicion(nodo_inicio_grafo, '@')
-			nodo_inicio_positiva.agregar_transicion(nodo_inicio_grafo, '@')
-			inicio.append(nodo_inicio_positiva)
-			final.append(nodo_final_positiva)
-			
-		elif expreg[i] == '*':
-			nodo_kleene_inicio = Nodo()
-			nodo_kleene_final = Nodo(True)
-			nodo_inicio_grafo = inicio.pop()
-			nodo_final_grafo = final.pop()
-			nodo_final_grafo.esFinal(False)
-			nodo_final_grafo.agregar_transicion(nodo_inicio_grafo, '@')
-			nodo_kleene_inicio.agregar_transicion(nodo_inicio_grafo, '@')
-			nodo_final_grafo.agregar_transicion(nodo_kleene_final, '@')
-			nodo_kleene_inicio.agregar_transicion(nodo_kleene_final, '@')
-			inicio.append(nodo_kleene_inicio)
-			final.append(nodo_kleene_final)
-			
-		else:
-			nuevoInicio = Nodo()
-			nuevoFinal = Nodo(True)
-			nuevoInicio.agregarTransicion(nuevoFinal, expreg[i])
-			
-			inicios.append(nuevoInicio)
-			finales.append(nuevoFinal)
-	
-	return (inicios[0], finales[0])
-	
-def main():
-	(a, b) = expregAThompson('ab-')
-	imprimirAutomata(a)
+    for i in range(n):
+        if expreg[i] == '|':
+            nuevoInicio = Nodo()      
+            nuevoFinal = Nodo(True)
+            
+            nodoInicioB = inicios.pop()
+            nodoInicioA = inicios.pop()
+            nodoFinalB = finales.pop()
+            nodoFinalA = finales.pop()
+            
+            nodoFinalA.esFinal = False
+            nodoFinalB.esFinal = False
+            nuevoInicio.agregarTransicion(nodoInicioA)
+            nuevoInicio.agregarTransicion(nodoInicioB)
+            nodoFinalA.agregarTransicion(nuevoFinal)
+            nodoFinalB.agregarTransicion(nuevoFinal)
+            
+            inicios.append(nuevoInicio)
+            finales.append(nuevoFinal)
 
-if __name__ == "__main__":
-	main()
+        elif expreg[i] == '-':
+            nodoInicioB = inicios.pop()
+            nodoInicioA = inicios.pop()
+            nodoFinalB = finales.pop()
+            nodoFinalA = finales.pop()
+
+            nodoFinalA.esFinal = False;
+            for transicion in nodoInicioB.transiciones:
+                nodoFinalA.agregarTransicion(transicion[0], transicion[1])
+            del nodoInicioB
+
+            inicios.append(nodoInicioA)
+            finales.append(nodoFinalB)
+
+        elif expreg[i] == '+' or expreg[i] == '*':
+            nuevoInicio = Nodo()
+            nuevoFinal = Nodo(True)
+            
+            nodoInicio = inicios.pop()
+            nodoFinal = finales.pop()
+            
+            nodoFinal.esFinal = False
+            nodoFinal.agregarTransicion(nuevoFinal)               
+            nodoFinal.agregarTransicion(nodoInicio)
+            nuevoInicio.agregarTransicion(nodoInicio)
+            
+            if expreg[i] == '*':
+                nuevoInicio.agregarTransicion(nuevoFinal)
+            
+            inicios.append(nuevoInicio)
+            finales.append(nuevoFinal)
+
+        else:
+            nuevoInicio = Nodo()
+            nuevoFinal = Nodo(True)
+            nuevoInicio.agregarTransicion(nuevoFinal, expreg[i])
+
+            inicios.append(nuevoInicio)
+            finales.append(nuevoFinal)
+
+    return inicios[0]
--- nodo.py
+++ nodo.py
@@ -1,20 +1,22 @@
+#!/usr/bin/python
+
 class Nodo:
-	identificador = 0
+    identificador = 0
 
-	def __init__(self, esFinal = False):
-		self.transiciones = []
-		self.esFinal = esFinal
-		self.id = Nodo.identificador
-		Nodo.identificador += 1
-		
-	def __str__(self):
-		return 'Nodo(%d)' % self.id
+    def __init__(self, esFinal = False):
+        self.transiciones = []
+        self.esFinal = esFinal
+        self.id = Nodo.identificador
+        Nodo.identificador += 1
 
-	def agregarTransicion(self, sig, char = '@'):
-		self.transiciones.append((sig, char))
+    def __str__(self):
+        return 'Nodo(%d)' % self.id
 
-	def imprimir(self):
-		print('Inicia ' + str(self))
-		for trans in self.transiciones:
-			print('%s --(%s)--> %s' % (str(self), trans[1], str(trans[0])))
-		print('Termina ' + str(self) + '\n')
+    def agregarTransicion(self, sig, char = '@'):
+        self.transiciones.append((sig, char))
+
+    def imprimir(self):
+        print('Inicia ' + str(self))
+        for trans in self.transiciones:
+            print('%s --(%s)--> %s' % (str(self), trans[1], str(trans[0])))
+        print('Termina ' + str(self) + '\n')
